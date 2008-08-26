7ddf285f86473a3288142ebca68840ef2c509e77
mike <>
2008-08-26 22:01:07 +0000

    Bind URI actions to commands on components

---
+++ source/spiralcraft/servlet/webui/components/ActionComponent.java
@@ -0,0 +1,175 @@
+//
+//Copyright (c) 1998,2008 Michael Toth
+//Spiralcraft Inc., All Rights Reserved
+//
+//This package is part of the Spiralcraft project and is licensed under
+//a multiple-license framework.
+//
+//You may not use this file except in compliance with the terms found in the
+//SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+//at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+//Unless otherwise agreed to in writing, this software is distributed on an
+//"AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.servlet.webui.components;
+
+import java.util.List;
+
+import spiralcraft.command.Command;
+import spiralcraft.lang.BindException;
+import spiralcraft.lang.Channel;
+import spiralcraft.lang.Expression;
+import spiralcraft.lang.Focus;
+
+import spiralcraft.servlet.webui.Action;
+import spiralcraft.servlet.webui.Component;
+import spiralcraft.servlet.webui.ServiceContext;
+
+import spiralcraft.text.markup.MarkupException;
+
+import spiralcraft.textgen.ElementState;
+import spiralcraft.textgen.EventContext;
+import spiralcraft.textgen.compiler.TglUnit;
+
+import spiralcraft.util.ArrayUtil;
+
+import spiralcraft.log.ClassLogger;
+
+/**
+ * <p>Registers an action that triggers a command, presents no output
+ * </p>
+ * 
+ * @author mike
+ *
+ */
+public class ActionComponent
+  extends Component
+{
+  
+  private static final ClassLogger log
+    =ClassLogger.getInstance(ActionComponent.class);
+  
+  private Expression<Command<?,?>> commandExpression;
+  private Channel<Command<?,?>> commandChannel;  
+  private String actionName;
+
+  public void setX(Expression<Command<?,?>> expression)
+  { commandExpression=expression;
+  }
+  
+  public void setActionName(String name)
+  { actionName=name;
+  }
+  
+  @Override
+  protected void handleInitialize(ServiceContext context)
+  {
+    super.handleInitialize(context);
+    if (actionName!=null)
+    { context.registerAction(createAction(context));
+    }
+    if (commandChannel!=null)
+    {
+      ((ActionState) context.getState())
+        .setCommand(commandChannel.get());
+    }
+    
+  }
+  
+  @Override
+  protected void handlePrepare(ServiceContext context)
+  {
+    super.handlePrepare(context);
+    if (actionName==null)
+    { context.registerAction(createAction(context));
+    }
+    
+    if (commandChannel!=null)
+    {
+      ((ActionState) context.getState())
+        .setCommand(commandChannel.get());
+    }
+  }
+  
+  @Override
+  public void bind(List<TglUnit> childUnits)
+    throws BindException,MarkupException
+  { 
+    
+    Focus<?> parentFocus=getParent().getFocus();
+    if (commandExpression!=null)
+    { commandChannel=parentFocus.bind(commandExpression);
+    }
+
+    bindChildren(childUnits);
+    
+  }  
+  
+  /**
+   * <p>Create an Action target
+   * </p>
+   * 
+   * @param context
+   * @return A new Action
+   */
+  protected Action createAction(EventContext context)
+  {
+    String actionName=this.actionName;
+    if (actionName==null)
+    { 
+      int[] path=context.getState().getPath();
+      actionName=ArrayUtil.format(path,".",null); 
+    }
+    
+    return new Action(actionName,context.getState().getPath())
+    {
+
+      { clearable=ActionComponent.this.actionName==null;
+      }
+      
+     
+      @Override
+      public void invoke(ServiceContext context)
+      { 
+        Command<?,?> command=((ActionState) context.getState()).getCommand();
+        if (command!=null)
+        {
+          command.execute();
+          if (command.getException()!=null)
+          { command.getException().printStackTrace();
+          }
+        }
+        else
+        { log.fine("No command associated with action "+getName());
+        }
+      }
+      
+    };
+  }  
+  
+  @Override
+  public ElementState createState()
+  { return new ActionState(this);
+  }
+  
+}
+
+class ActionState
+  extends ElementState
+{
+  private Command<?,?> command;
+  
+  public ActionState(ActionComponent comp)
+  { super(comp.getChildCount());
+  }
+  
+  public void setCommand(Command<?,?> command)
+  { this.command=command;
+  }
+  
+  public Command<?,?> getCommand()
+  { return command;
+  }
+}
+
