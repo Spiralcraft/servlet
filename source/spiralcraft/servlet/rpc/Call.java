ef218263836e57791c10852523ee4b74700f0c6c
mike <mike.git@spiralcraft.com>
2015-11-09 15:47:25 -0800

    respond method sends a coded response

--- source/spiralcraft/servlet/rpc/Call.java
+++ source/spiralcraft/servlet/rpc/Call.java
@@ -6,4 +6,10 @@ public class Call
   public final Response response=new Response();
   public final Request request=new Request();
   
+  public void respond(int statusCode,String message)
+  { 
+    response.setText(message);
+    response.setStatus(statusCode);
+  }
+  
 }
